4971506e867aa8edec26ae905ae1fe6fb42d413d
Arina-R <remizova.2002@list.ru>
2021-03-30 02:29:33 +0300

    first com

---
+++ Prakt/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Prakt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.l_1_Rasp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.but_Teacher = new System.Windows.Forms.Button();
+            this.l_1cl = new System.Windows.Forms.Label();
+            this.l_4cl = new System.Windows.Forms.Label();
+            this.l_2cl = new System.Windows.Forms.Label();
+            this.l_3cl = new System.Windows.Forms.Label();
+            this.but_1A = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // l_1_Rasp
+            // 
+            this.l_1_Rasp.AutoSize = true;
+            this.l_1_Rasp.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_1_Rasp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.l_1_Rasp.Location = new System.Drawing.Point(181, 31);
+            this.l_1_Rasp.Name = "l_1_Rasp";
+            this.l_1_Rasp.Size = new System.Drawing.Size(452, 84);
+            this.l_1_Rasp.TabIndex = 0;
+            this.l_1_Rasp.Text = " Расписание уроков\r\nдля начальных классов";
+            this.l_1_Rasp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Для редактирования:";
+            // 
+            // but_Teacher
+            // 
+            this.but_Teacher.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(185)))), ((int)(((byte)(220)))));
+            this.but_Teacher.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_Teacher.Location = new System.Drawing.Point(357, 385);
+            this.but_Teacher.Name = "but_Teacher";
+            this.but_Teacher.Size = new System.Drawing.Size(75, 23);
+            this.but_Teacher.TabIndex = 2;
+            this.but_Teacher.Text = "Войти";
+            this.but_Teacher.UseVisualStyleBackColor = false;
+            // 
+            // l_1cl
+            // 
+            this.l_1cl.AutoSize = true;
+            this.l_1cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_1cl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.l_1cl.Location = new System.Drawing.Point(95, 184);
+            this.l_1cl.Name = "l_1cl";
+            this.l_1cl.Size = new System.Drawing.Size(95, 24);
+            this.l_1cl.TabIndex = 3;
+            this.l_1cl.Text = "1 классы";
+            // 
+            // l_4cl
+            // 
+            this.l_4cl.AutoSize = true;
+            this.l_4cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_4cl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.l_4cl.Location = new System.Drawing.Point(614, 292);
+            this.l_4cl.Name = "l_4cl";
+            this.l_4cl.Size = new System.Drawing.Size(95, 24);
+            this.l_4cl.TabIndex = 4;
+            this.l_4cl.Text = "4 классы";
+            // 
+            // l_2cl
+            // 
+            this.l_2cl.AutoSize = true;
+            this.l_2cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_2cl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.l_2cl.Location = new System.Drawing.Point(614, 184);
+            this.l_2cl.Name = "l_2cl";
+            this.l_2cl.Size = new System.Drawing.Size(95, 24);
+            this.l_2cl.TabIndex = 5;
+            this.l_2cl.Text = "2 классы";
+            // 
+            // l_3cl
+            // 
+            this.l_3cl.AutoSize = true;
+            this.l_3cl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_3cl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.l_3cl.Location = new System.Drawing.Point(95, 292);
+            this.l_3cl.Name = "l_3cl";
+            this.l_3cl.Size = new System.Drawing.Size(95, 24);
+            this.l_3cl.TabIndex = 6;
+            this.l_3cl.Text = "3 классы";
+            // 
+            // but_1A
+            // 
+            this.but_1A.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(63)))));
+            this.but_1A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_1A.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.but_1A.Location = new System.Drawing.Point(67, 222);
+            this.but_1A.Name = "but_1A";
+            this.but_1A.Size = new System.Drawing.Size(45, 23);
+            this.but_1A.TabIndex = 7;
+            this.but_1A.Text = "1 A";
+            this.but_1A.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(165)))), ((int)(((byte)(225)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.but_1A);
+            this.Controls.Add(this.l_3cl);
+            this.Controls.Add(this.l_2cl);
+            this.Controls.Add(this.l_4cl);
+            this.Controls.Add(this.l_1cl);
+            this.Controls.Add(this.but_Teacher);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.l_1_Rasp);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Расписание уроков";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_1_Rasp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button but_Teacher;
+        private System.Windows.Forms.Label l_1cl;
+        private System.Windows.Forms.Label l_4cl;
+        private System.Windows.Forms.Label l_2cl;
+        private System.Windows.Forms.Label l_3cl;
+        private System.Windows.Forms.Button but_1A;
+    }
+}
+
